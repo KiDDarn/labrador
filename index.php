34165a55c8bc6d92f599762c1f2df7ec6d940181
Mattias Petersson <93470851+KiDDarn@users.noreply.github.com>
2025-05-22 22:53:42 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>m4p3.nu: Maintenance..</title>
+
+    <meta property="og:title" content="m4p3.nu - A TECH SOLUTION & CREATIVE DIGITAL AGENCY"/>
+    <meta property="og:url" content="http://m4p3.nu"/>
+    <meta property="og:image" content=""/>
+    <meta property="og:description" content="A TECH SOLUTION & CREATIVE DIGITAL AGENCY"/>
+
+    <meta itemprop="name" content="m4p3.nu - A TECH SOLUTION & CREATIVE DIGITAL AGENCY"/>
+    <meta itemprop="description" content="A TECH SOLUTION & CREATIVE DIGITAL AGENCY"/>
+    <link href='img/logo-suaiq.png' rel='icon' type='image/x-icon'/>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/landing-page.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+</head>
+
+<body class="site">
+    <div class="overlay">
+        <!-- Header -->
+        <div class="container">
+            <div class="intro-message">
+                <h1>m4p3.nu</h1>
+                <h2>Our website is currently under construction.</h2>
+                <hr class="intro-divider">
+                <h3>Stay tuned for something amazing!</br>
+                </h3>
+                <ul class="intro-social-buttons">
+				</ul>
+            </div>
+            <div class="intro-footer">
+                <p class="copyright text-muted small">Copyright &copy; <?php echo date("Y"); ?>. All Rights Reserved</p>
+            </div>
+        </div>
+        <!-- /.container -->
+    </div>
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
